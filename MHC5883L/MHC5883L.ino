741a6ca98547376c9318a8a9b14f4e14e7ac8e52
jaapiyo <jaap@blinkinglights.nl>
2017-05-08 13:29:04 +0200

    datasheets added

--- MHC5883L/MHC5883L.ino
+++ MHC5883L/MHC5883L.ino
@@ -1,3 +1,14 @@
+
+// Practicum positiebepaling
+// HMC5883L 3-axis MEMS compass
+// 8-5-2017
+//
+// Jaap Elstgeest en Jelle Kampf
+
+#include Wire.h
+
+
+#include <Wire.h>
 void setup()
 {
 
--- MHC5883L/__vm/.MHC5883L.vsarduino.h
+++ MHC5883L/__vm/.MHC5883L.vsarduino.h
@@ -5,55 +5,77 @@
 			all non-arduino files created by visual micro and all visual studio project or solution files can be freely deleted and are not required to compile a sketch (do not delete your own code!).
 			note: debugger breakpoints are stored in '.sln' or '.asln' files, knowledge of last uploaded breakpoints is stored in the upload.vmps.xml file. Both files are required to continue a previous debug session without needing to compile and upload again
 	
-	Hardware: Teensy 3.5, Platform=teensy3, Package=teensy
+	Hardware: Arduino/Genuino Uno, Platform=avr, Package=arduino
 */
 
 #if defined(_VMICRO_INTELLISENSE)
 
 #ifndef _VSARDUINO_H_
 #define _VSARDUINO_H_
-#define __HARDWARE_MK64fx512__
-#define __HARDWARE_MK64FX512__
-#define __MK64FX512__
-#define TEENSYDUINO 131
+#define __AVR_ATmega328p__
+#define __AVR_ATmega328P__
+#define F_CPU 16000000L
 #define ARDUINO 106012
-#define F_CPU 120000000
-#define USB_SERIAL
-#define LAYOUT_US_ENGLISH
+#define ARDUINO_AVR_UNO
+#define ARDUINO_ARCH_AVR
 #define __cplusplus 201103L
-#define __arm__
-#define __ARM__
-#define __extension__
-#define  __attribute__(x)
-typedef void *__builtin_va_list;
-#define __extension__
-#define __ATTR_PURE__
-#define __ATTR_CONST__
+#define __AVR__
 #define __inline__
-#define __asm__ 
+#define __asm__(...)
+#define __extension__
+#define __inline__
 #define __volatile__
-#define _HAVE_STDC
+#define GCC_VERSION 40902
+
+#define __cplusplus 201103L
+#undef __cplusplus
+#define __cplusplus 201103L
+
+#define volatile(va_arg) 
+#define _CONST
+#define __builtin_va_start
+#define __builtin_va_end
+#define __attribute__(...)
+#define NOINLINE __attribute__((noinline))
+#define prog_void
+#define PGM_VOID_P int
+
+
+#ifndef __builtin_constant_p
+	#define __builtin_constant_p __attribute__((__const__))
+#endif
+#ifndef __builtin_strlen
+	#define __builtin_strlen  __attribute__((__const__))
+#endif
+
 
 #define NEW_H
+typedef void *__builtin_va_list;
+//extern "C" void __cxa_pure_virtual() {;}
+
+typedef int div_t;
+typedef int ldiv_t;
+
+
+typedef void *__builtin_va_list;
+//extern "C" void __cxa_pure_virtual() {;}
+
+
 
 #include <arduino.h>
-#define __arm__
-#define __ARM__
-#define __extension__
-#define  __attribute__(x)
-typedef void *__builtin_va_list;
-#define __extension__
-#define __ATTR_PURE__
-#define __ATTR_CONST__
-#define __inline__
-#define __asm__ 
-#define __volatile__
-
-#define __disable_irq() __asm__ volatile("");
-#define __enable_irq()	__asm__ volatile("");
+#include <pins_arduino.h> 
+#undef F
+#define F(string_literal) ((const PROGMEM char *)(string_literal))
+#undef PSTR
+#define PSTR(string_literal) ((const PROGMEM char *)(string_literal))
 
 
-#define NEW_H
+#define pgm_read_byte(address_short) uint8_t() 
+#define pgm_read_word(address_short) uint16_t() 
+#define pgm_read_dword(address_short) uint32_t()
+#define pgm_read_float(address_short) float()
+#define pgm_read_ptr(address_short)   short()
+
 #include "MHC5883L.ino"
 #endif
 #endif
